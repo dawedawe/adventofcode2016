292bc83f6738af92c5a579bfef637bbb65ff4343
dawe <dawedawe@gmx.de>
2020-07-19 23:45:43 +0200

    Day 03 Part 2

--- adventofcode/Day03.fs
+++ adventofcode/Day03.fs
@@ -20,3 +20,21 @@ module Day03 =
         getInput InputFile
         |> Array.filter isLegal
         |> Array.length
+
+    let day03Part2() =
+        let lines = getInput InputFile
+        let tris = seq {
+            for i in [0 .. 3 .. (lines.Length - 3)] do
+                let line0 = lines.[i]
+                let line1 = lines.[i+1]
+                let line2 = lines.[i+2]
+                let tri0 = [| line0.[0]; line1.[0]; line2.[0]  |]
+                let tri1 = [| line0.[1]; line1.[1]; line2.[1]  |]
+                let tri2 = [| line0.[2]; line1.[2]; line2.[2]  |]
+                [| tri0; tri1; tri2 |]
+        }
+        tris
+        |> Array.ofSeq
+        |> Array.collect id
+        |> Array.filter isLegal
+        |> Array.length
