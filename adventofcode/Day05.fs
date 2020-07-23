20589eabe3e99d117014ec7519cf06674d0bc363
dawe <dawedawe@gmx.de>
2020-07-23 22:33:14 +0200

    Day 05 Part 2

--- adventofcode/Day05.fs
+++ adventofcode/Day05.fs
@@ -17,3 +17,18 @@ module Day05 =
             if (h.StartsWith("00000")) then pw <- pw + string h.[5]
             i <- i + 1
         pw
+
+    let day05Part2() =
+        let md5 = System.Security.Cryptography.MD5.Create()
+        let mutable i = 0
+        let mutable pw = "________".ToCharArray()
+        while (Array.contains '_' pw) do
+            let h = hash md5 ("abbhdwsy" + string i)
+            if (h.StartsWith("00000") && System.Char.IsNumber h.[5]) then
+                let index = System.Int32.Parse(string h.[5])
+                if (index < 8 && pw.[index] = '_') then
+                    pw.[index] <- h.[6]
+                    printfn "%A" pw
+
+            i <- i + 1
+        pw |> Array.fold (fun s c -> s + string c) ""
