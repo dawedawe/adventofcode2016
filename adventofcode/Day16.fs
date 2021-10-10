646afbc7f26e48c35de019f6c1a9f516ba79a688
dawedawe <dawedawe@posteo.de>
2021-10-10 23:21:33 +0200

    Day 16 Part 1

---
+++ adventofcode/Day16.fs
@@ -0,0 +1,36 @@
+namespace Adventofcode2016
+
+module Day16 =
+
+    let Input = ("01000100010010111", 272)
+
+    let comp c = if c = '1' then '0' else '1'
+
+    let rec expand bits length =
+        let a = bits
+        let b = a
+        let b' = Array.rev b
+        let b'' = b' |> Array.map comp
+        let r = Array.concat [ a; [| '0' |]; b'' ]
+
+        if r.Length >= length then
+            Array.take length r
+        else
+            expand r length
+
+    let rec checksum bits =
+        let reduction =
+            [| for i in 0 .. 2 .. (Array.length bits - 2) do
+                   if bits.[i] = bits.[i + 1] then
+                       '1'
+                   else
+                       '0' |]
+
+        if reduction.Length % 2 = 0 then
+            checksum reduction
+        else
+            System.String(reduction)
+
+    let day16 () =
+        expand ((fst Input).ToCharArray()) (snd Input)
+        |> checksum
