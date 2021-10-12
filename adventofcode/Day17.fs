c5cba41c92809528591cf6c96adf39ef88390355
dawedawe <dawedawe@posteo.de>
2021-10-13 00:04:44 +0200

    Day 17 Part 1

---
+++ adventofcode/Day17.fs
@@ -0,0 +1,80 @@
+namespace Adventofcode2016
+
+module Day17 =
+
+    let Input = "mmsxrhfx"
+
+    type Node = { Path: string; Pos: int * int }
+
+    let md5 =
+        System.Security.Cryptography.MD5.Create()
+
+    let hash input path =
+        let data = sprintf "%s%s" input path
+
+        let hash =
+            md5.ComputeHash(System.Text.Encoding.ASCII.GetBytes(data))
+
+        sprintf "%02x%02x" hash.[0] hash.[1]
+
+    let isDoorOpen =
+        function
+        | 'b'
+        | 'c'
+        | 'd'
+        | 'e'
+        | 'f' -> true
+        | _ -> false
+
+    let getDoorStates (h: string) =
+        let up = isDoorOpen h.[0]
+        let down = isDoorOpen h.[1]
+        let left = isDoorOpen h.[2]
+        let right = isDoorOpen h.[3]
+        (up, down, left, right)
+
+    let getValidDirections (x, y) (u, d, l, r) =
+        seq {
+            if y > 0 && u then yield "U"
+            if y < 3 && d then yield "D"
+            if x > 0 && l then yield "L"
+            if x < 3 && r then yield "R"
+        }
+
+    let move (x, y) =
+        function
+        | "U" -> (x, y - 1)
+        | "D" -> (x, y + 1)
+        | "L" -> (x - 1, y)
+        | "R" -> (x + 1, y)
+        | _ -> failwith "bad direction"
+
+    let bfs () =
+        let goal = (3, 3)
+        let queue = System.Collections.Generic.Queue<Node>()
+        let mutable pathFound = None
+        queue.Enqueue({ Path = ""; Pos = (0, 0) })
+
+        while queue.Count > 0 && Option.isNone pathFound do
+            let node = queue.Dequeue()
+
+            if node.Pos = goal then
+                pathFound <- Some node.Path
+            else
+                let dirs =
+                    hash Input node.Path
+                    |> getDoorStates
+                    |> getValidDirections node.Pos
+
+                for d in dirs do
+                    let newPos = move node.Pos d
+                    let newPath = node.Path + d
+                    let newNode = { Path = newPath; Pos = newPos }
+                    queue.Enqueue(newNode)
+
+        if Option.isSome pathFound then
+            pathFound.Value
+        else
+            failwith "no path found"
+
+    let day17 () = bfs ()
