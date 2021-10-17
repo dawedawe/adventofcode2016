151c4c09c5e0ac537f0da353ef90ade18347eaf1
dawedawe <dawedawe@posteo.de>
2021-10-17 21:13:43 +0200

    Day 19 Part 2

--- adventofcode/Day19.fs
+++ adventofcode/Day19.fs
@@ -16,8 +16,17 @@ module Day19 =
             circle
             |> Map.findKey (fun id c -> id < currentId && c > 0)
 
-    let rec turn input currentId (circle: Map<int, int>) =
-        let posToRob = getIdToTheLeft currentId circle
+    let getIdAcross currentId (circle: Map<int, int>) =
+        let stillInGameCircle = circle |> Map.filter (fun _ c -> c > 0)
+        let stillInGameCount = stillInGameCircle |> Map.count
+        let distance = stillInGameCount / 2
+        let keys = stillInGameCircle |> Map.toList |> List.map fst
+        let indexOfCurrentId = keys |> List.findIndex (fun id -> id = currentId)
+        let idAcross = (indexOfCurrentId + distance) % stillInGameCount
+        keys.[idAcross]
+
+    let rec turn getNextId getIdToRob input currentId (circle: Map<int, int>) =
+        let posToRob = getIdToRob currentId circle
 
         let circle' =
             circle
@@ -25,10 +34,10 @@ module Day19 =
             |> Map.add posToRob 0
 
         if circle'.[currentId] = input then
-            currentId + 1
+            currentId
         else
-            let currentId' = getIdToTheLeft currentId circle'
-            turn input currentId' circle'
+            let currentId' = getNextId currentId circle'
+            turn getNextId getIdToRob input currentId' circle'
 
     let day19 () =
 
@@ -39,3 +48,27 @@ module Day19 =
             pos <- if pos' > i then pos' % i else pos'
 
         pos
+
+    let day19Part2 () =
+
+        let mutable pos = 0
+        let mutable increments = 1
+        let mutable i = 2
+        let mutable result = 0
+        while i <= Input do
+
+            for j in 1 .. increments do
+                let pos' = pos + 1
+                pos <- if pos' >= i then 1 else pos'
+                if i = Input then result <- pos
+                i <- i + 1
+
+            for j in 1 .. increments do
+                let pos' = pos + 2
+                pos <- if pos' > i then pos' % i else pos'
+                if i = Input then result <- pos
+                i <- i + 1
+
+            increments <- increments * 3
+
+        result
